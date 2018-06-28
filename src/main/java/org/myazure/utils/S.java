f72a670a176bd7a5d1a553991052f2d10305ef98
WangZhen <wangzhenjjcn@gmail.com>
2018-06-29 01:03:16 +0800

    add random

--- src/main/java/org/myazure/utils/S.java
+++ src/main/java/org/myazure/utils/S.java
@@ -24,4 +24,27 @@ public class S {
 		}
 		return 0;
 	}
+	
+	public static String getRandomString(int number) {
+		if (number > 0) {
+			StringBuffer charsBuffer = new StringBuffer();
+			String[] chars={"1","2","3","4","5","6","7","8","9","0","q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m","Q","W","E","R","T","Y","U","I","O","P","A","S","D","F","G","H","J","K","L","Z","X","C","V","B","N","M"};
+			int num = (int) (Math.random() * 100L);
+			for (int i = 0; i < number;   num = (int) (Math.random() * 10L)) {
+				if (num>=0 && num <chars.length) {
+					charsBuffer.append(chars[num]);
+					i++;
+				} 
+			}
+			return charsBuffer.toString();
+		} else {
+			return "0";
+		}
+	}
+	
+	
+	
+	
+	
+	
 }
