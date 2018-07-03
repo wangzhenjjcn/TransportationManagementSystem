55109372319a8eec0b7fc3a917774d548c05e25c
WangZhen <wangzhenjjcn@gmail.com>
2018-07-04 05:58:07 +0800

    add

--- src/main/java/org/myazure/domain/Order.java
+++ src/main/java/org/myazure/domain/Order.java
@@ -1,10 +1,13 @@
 package org.myazure.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -13,6 +16,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Transient;
 
@@ -41,9 +45,9 @@ public class Order extends BaseEntity implements Serializable {
 	@Column(name = "order_date", columnDefinition = "datetime DEFAULT NULL")
 	@JSONField(name = "orderDate")
 	private Date orderDate;
-	@JSONField(name = "creat_user")
-	@JsonProperty("creat_user")
-	@JoinColumn(name = "creat_user_id")
+	@JSONField(name = "creator_user")
+	@JsonProperty("creator_user")
+	@JoinColumn(name = "creator_user_id")
 	@ManyToOne(targetEntity = WebUser.class, fetch = FetchType.LAZY)
 	private WebUser creatorUser = new WebUser();
 	@JSONField(name = "customer_user")
@@ -89,7 +93,6 @@ public class Order extends BaseEntity implements Serializable {
 	@JSONField(name = "factory_address")
 	@JsonProperty("factory_address")
 	private String factoryAddress;
-	// `from` varchar(255) DEFAULT NULL,
 	@JsonProperty("source")
 	@Column(name = "source", columnDefinition = "varchar(255) DEFAULT NULL")
 	@JSONField(name = "source")
@@ -98,26 +101,26 @@ public class Order extends BaseEntity implements Serializable {
 	@JSONField(name = "sourcepy")
 	@Column(name = "sourcepy", columnDefinition = "varchar(25) DEFAULT NULL")
 	private String frompy;
-	// `to` varchar(255) DEFAULT NULL,
 	@JsonProperty("destination")
 	@Column(name = "destination", columnDefinition = "varchar(255) DEFAULT NULL")
 	@JSONField(name = "destination")
 	private String destination;
-	// `to` varchar(255) DEFAULT NULL,
 	@JsonProperty("destinationpy")
 	@Column(name = "destinationpy", columnDefinition = "varchar(25) DEFAULT NULL")
 	@JSONField(name = "destinationpy")
 	private String destinationpy;
-	@Transient
+	@JsonProperty("transfer_number")
+	@Column(name = "transfer_number", columnDefinition = "varchar(255) DEFAULT NULL")
+	@JSONField(name = "transfer_number")
 	private String transferNumber;
-	@Transient
+	@JsonProperty("pickup_number")
+	@Column(name = "pickup_number", columnDefinition = "varchar(255) DEFAULT NULL")
+	@JSONField(name = "pickup_number")
 	private String pickUpNumber;
-	// `entry_number` varchar(255) DEFAULT NULL,
 	@JsonProperty("entry_number")
 	@Column(name = "entry_number", columnDefinition = "varchar(255) DEFAULT NULL")
 	@JSONField(name = "entry_number")
 	private String entryNumber;
-	// `packages` int(11) DEFAULT NULL,
 	@JsonProperty("packages")
 	@Column(name = "packages", columnDefinition = "int(11) DEFAULT NULL")
 	@JSONField(name = "packages")
@@ -126,12 +129,10 @@ public class Order extends BaseEntity implements Serializable {
 	@Column(name = "weight", columnDefinition = "int(11) DEFAULT NULL")
 	@JSONField(name = "weight")
 	private int weight;
-	// `size` int(11) DEFAULT NULL,
 	@JsonProperty("size")
 	@Column(name = "size", columnDefinition = "int(11) DEFAULT NULL")
 	@JSONField(name = "size")
 	private int size;
-	// `distence` int(11) DEFAULT NULL,
 	@JsonProperty("distence")
 	@Column(name = "distence", columnDefinition = "int(11) DEFAULT NULL")
 	@JSONField(name = "distence")
@@ -148,6 +149,10 @@ public class Order extends BaseEntity implements Serializable {
 	@Column(name = "remarks", columnDefinition = "varchar(255) DEFAULT NULL")
 	@JSONField(name = "remarks")
 	private String remarks;
+	@JsonProperty("remarkspy")
+	@Column(name = "remarkspy", columnDefinition = "varchar(255) DEFAULT NULL")
+	@JSONField(name = "remarkspy")
+	private String remarkspy;
 	@JsonProperty("transport_vehicle_registration_number")
 	@Column(name = "transport_vehicle_registration_number", columnDefinition = "varchar(9) DEFAULT NULL")
 	@JSONField(name = "transport_vehicle_registration_number")
@@ -156,59 +161,63 @@ public class Order extends BaseEntity implements Serializable {
 	@Column(name = "delivery_vehicle_registration_number", columnDefinition = "varchar(9) DEFAULT NULL")
 	@JSONField(name = "delivery_vehicle_registration_number")
 	private String deliveryVehicleRegistrationNumber;
+	@JSONField(name = "customer")
+	@JsonProperty("customer")
 	@JoinColumn(name = "customer_id")
-	@JSONField(name = "customer_id")
 	@ManyToOne(targetEntity = Customer.class, fetch = FetchType.LAZY)
-	@JsonProperty("customer_id")
 	private Customer customer;
-	// `plan_id` bigint(11) DEFAULT NULL,
 	@JsonProperty("planId")
 	@JSONField(name = "planId")
 	@JoinColumn(name = "plan_id", columnDefinition = "bigint(11) DEFAULT NULL")
 	@ManyToOne(targetEntity = Plan.class, fetch = FetchType.LAZY)
 	private Plan plan;
-	// `customer_number` varchar(30) DEFAULT NULL,
 	@JsonProperty("customer_number")
 	@Column(name = "customer_number", columnDefinition = "varchar(30) DEFAULT NULL")
 	@JSONField(name = "customer_number")
 	private String customerNumber;
-	@Transient
-	private boolean isFreight;
-	// `contact` varchar(255) DEFAULT NULL,
+	@JsonProperty("freight_tpye")
+	@JSONField(name = "freight_tpye")
+	@Column(name = "freight_tpye", columnDefinition = " int(1) DEFAULT '0'")
+	private int freightType;
 	@JsonProperty("contact")
 	@Column(name = "contact", columnDefinition = "varchar(255) DEFAULT NULL")
 	@JSONField(name = "contact")
 	private String contactName;
-	// `phone` varchar(255) DEFAULT NULL,
+	@JsonProperty("contactpy")
+	@Column(name = "contactpy", columnDefinition = "varchar(255) DEFAULT NULL")
+	@JSONField(name = "contactpy")
+	private String contactNamepy;
 	@JsonProperty("phone")
 	@Column(name = "phone", columnDefinition = "varchar(255) DEFAULT NULL")
 	@JSONField(name = "phone")
 	private String contactPhone;
-	// `isCharterd` int(11) DEFAULT NULL,
-	@JsonProperty("isCharterd")
-	@Column(name = "isCharterd", columnDefinition = "int(11) DEFAULT NULL")
-	@JSONField(name = "isCharterd")
-	private boolean isCharterd;
+	@JsonProperty("isChartered")
+	@Column(name = "isChartered", columnDefinition = "bit(1) DEFAULT NULL")
+	@JSONField(name = "isChartered")
+	private boolean isChartered = false;
 	@JsonProperty("fee_time")
 	@JSONField(name = "fee_time")
 	@Column(name = "fee_time", columnDefinition = "int(2) DEFAULT NULL")
-	private int feeTime;
-	// 0创建
-	// 1审核
-	// 2运输
-	// 3送达
-	// 4上传
-	// 5完成
-	// 6结算
-	// 7关闭
+	private int feeTime = 1;
 	@JsonProperty("order_state")
-	@Column(name = "order_state", columnDefinition = "int(2) unsigned DEFAULT '0'")
 	@JSONField(name = "order_state")
+	@Column(name = "order_state", columnDefinition = "int(2) unsigned DEFAULT '0'")
 	private int orderState = 0;
+	// 0草稿 // 1审核 // 2运输 // 3送达 // 4上传 // 5完成 // 6结算 // 7关闭
 	@Transient
+	@JsonProperty("order_state_string")
+	@JSONField(name = "order_state_string")
 	private String orderStateString;
 	@Transient
-	private Map<String, Integer> feeHistory = new HashMap<String, Integer>();
+	@JsonProperty("order_state_string_py")
+	@JSONField(name = "order_state_string_py")
+	private String orderStateStringPY;
+	@JSONField(name = "payments")
+	@JsonProperty("payments")
+	@OneToMany(cascade = CascadeType.ALL, targetEntity = Payment.class, fetch = FetchType.LAZY)
+	@JoinColumn(name = "order_id")
+	// @ManyToOne(targetEntity = Payment.class, fetch = FetchType.LAZY)
+	private List<Payment> feeHistory = new ArrayList<Payment>();
 
 	public Order() {
 		orderCreatDate = new Date(System.currentTimeMillis());
@@ -216,332 +225,6 @@ public class Order extends BaseEntity implements Serializable {
 		contactName = "";
 		orderState = 0;
 		orderStateString = "新订单";
-	}
-
-
-	public Long getOrderId() {
-		return orderId;
-	}
-
-
-	public void setOrderId(Long orderId) {
-		this.orderId = orderId;
-	}
-
-
-	public Date getOrderCreatDate() {
-		return orderCreatDate;
-	}
-
-	public void setOrderCreatDate(Date orderCreatDate) {
-		this.orderCreatDate = orderCreatDate;
-	}
-
-	public Date getOrderDate() {
-		return orderDate;
-	}
-
-	public void setOrderDate(Date orderDate) {
-		this.orderDate = orderDate;
-	}
-
-	public WebUser getCreatorUser() {
-		return creatorUser;
-	}
-
-	public void setCreatorUser(WebUser creatorUser) {
-		this.creatorUser = creatorUser;
-	}
-
-	public WebUser getCustomerUser() {
-		return customerUser;
-	}
-
-	public void setCustomerUser(WebUser customerUser) {
-		this.customerUser = customerUser;
-	}
-
-	public WebUser getTransportDriverUser() {
-		return transportDriverUser;
-	}
-
-	public void setTransportDriverUser(WebUser transportDriverUser) {
-		this.transportDriverUser = transportDriverUser;
-	}
-
-	public WebUser getDeliveryDriverUser() {
-		return deliveryDriverUser;
-	}
-
-	public void setDeliveryDriverUser(WebUser deliveryDriverUser) {
-		this.deliveryDriverUser = deliveryDriverUser;
-	}
-
-	public WebUser getFactoryUser() {
-		return factoryUser;
-	}
-
-	public void setFactoryUser(WebUser factoryUser) {
-		this.factoryUser = factoryUser;
-	}
-
-	public Vehicle getTransportVehicle() {
-		return transportVehicle;
-	}
-
-	public void setTransportVehicle(Vehicle transportVehicle) {
-		this.transportVehicle = transportVehicle;
-	}
-
-	public Vehicle getDeliveryVehicle() {
-		return deliveryVehicle;
-	}
-
-	public void setDeliveryVehicle(Vehicle deliveryVehicle) {
-		this.deliveryVehicle = deliveryVehicle;
-	}
-
-	public Factory getFactory() {
-		return factory;
-	}
-
-	public void setFactory(Factory factory) {
-		this.factory = factory;
-	}
-
-
-	public String getFactoryName() {
-		return factoryName;
-	}
-
-	public void setFactoryName(String factoryName) {
-		this.factoryName = factoryName;
-	}
-
-	public String getFactoryAddress() {
-		return factoryAddress;
-	}
-
-	public void setFactoryAddress(String factoryAddress) {
-		this.factoryAddress = factoryAddress;
-	}
-
-	public String getFrom() {
-		return from;
-	}
-
-	public void setFrom(String from) {
-		this.from = from;
-	}
-
-	public String getFrompy() {
-		return frompy;
-	}
-
-	public void setFrompy(String frompy) {
-		this.frompy = frompy;
-	}
-
-	public String getDestination() {
-		return destination;
-	}
-
-	public void setDestination(String destination) {
-		this.destination = destination;
-	}
-
-	public String getDestinationpy() {
-		return destinationpy;
-	}
-
-	public void setDestinationpy(String destinationpy) {
-		this.destinationpy = destinationpy;
-	}
-
-	public String getTransferNumber() {
-		return transferNumber;
-	}
-
-	public void setTransferNumber(String transferNumber) {
-		this.transferNumber = transferNumber;
-	}
-
-	public String getPickUpNumber() {
-		return pickUpNumber;
-	}
-
-	public void setPickUpNumber(String pickUpNumber) {
-		this.pickUpNumber = pickUpNumber;
-	}
-
-	public String getEntryNumber() {
-		return entryNumber;
-	}
-
-	public void setEntryNumber(String entryNumber) {
-		this.entryNumber = entryNumber;
-	}
-
-	public int getPakagesNumber() {
-		return pakagesNumber;
-	}
-
-	public void setPakagesNumber(int pakagesNumber) {
-		this.pakagesNumber = pakagesNumber;
-	}
-
-	public int getWeight() {
-		return weight;
-	}
-
-	public void setWeight(int weight) {
-		this.weight = weight;
-	}
-
-	public int getSize() {
-		return size;
-	}
-
-	public void setSize(int size) {
-		this.size = size;
-	}
-
-	public int getDistence() {
-		return distence;
-	}
-
-	public void setDistence(int distence) {
-		this.distence = distence;
-	}
-
-	public int getCarriageFee() {
-		return carriageFee;
-	}
-
-	public void setCarriageFee(int carriageFee) {
-		this.carriageFee = carriageFee;
-	}
-
-	public int getCushionFee() {
-		return cushionFee;
-	}
-
-	public void setCushionFee(int cushionFee) {
-		this.cushionFee = cushionFee;
-	}
-
-	public String getRemarks() {
-		return remarks;
-	}
-
-	public void setRemarks(String remarks) {
-		this.remarks = remarks;
-	}
-
-	public String getTransportVehicleRegistrationNumber() {
-		return transportVehicleRegistrationNumber;
-	}
-
-	public void setTransportVehicleRegistrationNumber(
-			String transportVehicleRegistrationNumber) {
-		this.transportVehicleRegistrationNumber = transportVehicleRegistrationNumber;
-	}
-
-	public String getDeliveryVehicleRegistrationNumber() {
-		return deliveryVehicleRegistrationNumber;
-	}
-
-	public void setDeliveryVehicleRegistrationNumber(
-			String deliveryVehicleRegistrationNumber) {
-		this.deliveryVehicleRegistrationNumber = deliveryVehicleRegistrationNumber;
-	}
-
-	public Customer getCustomer() {
-		return customer;
-	}
-
-	public void setCustomer(Customer customer) {
-		this.customer = customer;
-	}
-
-	public Plan getPlan() {
-		return plan;
-	}
-
-	public void setPlan(Plan plan) {
-		this.plan = plan;
-	}
-
-	public String getCustomerNumber() {
-		return customerNumber;
-	}
-
-	public void setCustomerNumber(String customerNumber) {
-		this.customerNumber = customerNumber;
-	}
-
-	public boolean isFreight() {
-		return isFreight;
-	}
-
-	public void setFreight(boolean isFreight) {
-		this.isFreight = isFreight;
-	}
-
-	public String getContactName() {
-		return contactName;
-	}
-
-	public void setContactName(String contactName) {
-		this.contactName = contactName;
-	}
-
-	public String getContactPhone() {
-		return contactPhone;
-	}
-
-	public void setContactPhone(String contactPhone) {
-		this.contactPhone = contactPhone;
-	}
-
-	public boolean isCharterd() {
-		return isCharterd;
-	}
-
-	public void setCharterd(boolean isCharterd) {
-		this.isCharterd = isCharterd;
-	}
-
-	public int getFeeTime() {
-		return feeTime;
-	}
-
-	public void setFeeTime(int feeTime) {
-		this.feeTime = feeTime;
-	}
-
-	public int getOrderState() {
-		return orderState;
-	}
-
-	public void setOrderState(int orderState) {
-		this.orderState = orderState;
-	}
-
-	public String getOrderStateString() {
-		return orderStateString;
-	}
-
-	public void setOrderStateString(String orderStateString) {
-		this.orderStateString = orderStateString;
-	}
-
-	public Map<String, Integer> getFeeHistory() {
-		return feeHistory;
-	}
-
-	public void setFeeHistory(Map<String, Integer> feeHistory) {
-		this.feeHistory = feeHistory;
 	}
 
 	public static Order getOrderExp() {
@@ -593,12 +276,9 @@ public class Order extends BaseEntity implements Serializable {
 		order.setDeliveryVehicle(deliveryVehicle);
 		Factory factory = new Factory();
 		Map<String, Integer> feeHistory = new HashMap<String, Integer>();
-		feeHistory.put("代付费",
-				S.getRandomNum(2));
-		feeHistory.put("过路费",
-				S.getRandomNum(2));
-		feeHistory.put("其他",
-				S.getRandomNum(2));
+		feeHistory.put("代付费", S.getRandomNum(2));
+		feeHistory.put("过路费", S.getRandomNum(2));
+		feeHistory.put("其他", S.getRandomNum(2));
 		order.setFeeHistory(feeHistory);
 		order.setFactory(factory);
 		Vehicle transportVehicle = new Vehicle();
---
+++ src/main/java/org/myazure/domain/Payment.java
@@ -0,0 +1,128 @@
+package org.myazure.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import com.alibaba.fastjson.annotation.JSONField;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@Entity
+@Table(name = "payment")
+public class Payment  extends BaseEntity implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@JsonProperty("pay_id")
+	@JSONField(name = "pay_id")
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "pay_id", nullable = false, length = 11, columnDefinition = "bigint(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '订单编号'", insertable = true)
+	private Long payId;
+	 
+	@JsonProperty("order_id")
+	@JSONField(name = "order_id")
+	@Column(name = "order_id", columnDefinition = "bigint(11) unsigned NOT NULL")
+	private Long orderId;
+	
+	@JsonProperty("payout")
+	@Column(name = "payout", columnDefinition = "bigint(11) unsigned NOT NULL")
+	@JSONField(name = "payout")
+	private int payout ;
+	
+	@JsonProperty("pay_time")
+	@Column(name = "pay_time", columnDefinition = "datetime DEFAULT NULL")
+	@JSONField(name = "pay_time")
+	private Date payTime;
+	
+	@JSONField(name = "creator_user")
+	@JsonProperty("creator_user")
+	@JoinColumn(name = "creator_user_id")
+	@ManyToOne(targetEntity = WebUser.class, fetch = FetchType.LAZY)
+	private WebUser creatorUser = new WebUser();
+	
+	@JsonProperty("remarks")
+	@Column(name = "remarks", columnDefinition = "varchar(255) DEFAULT NULL")
+	@JSONField(name = "remarks")
+	private String remarks;
+	
+	
+	@JsonProperty("remarkspy")
+	@Column(name = "remarkspy", columnDefinition = "varchar(255) DEFAULT NULL")
+	@JSONField(name = "remarkspy")
+	private String remarkspy;
+	
+	public Payment(){
+		
+	}
+
+	public Long getPayId() {
+		return payId;
+	}
+
+	public void setPayId(Long payId) {
+		this.payId = payId;
+	}
+
+	public Long getOrderId() {
+		return orderId;
+	}
+
+	public void setOrderId(Long orderId) {
+		this.orderId = orderId;
+	}
+
+	public int getPayout() {
+		return payout;
+	}
+
+	public void setPayout(int payout) {
+		this.payout = payout;
+	}
+
+	public Date getPayTime() {
+		return payTime;
+	}
+
+	public void setPayTime(Date payTime) {
+		this.payTime = payTime;
+	}
+
+	public WebUser getCreatorUser() {
+		return creatorUser;
+	}
+
+	public void setCreatorUser(WebUser creatorUser) {
+		this.creatorUser = creatorUser;
+	}
+
+	public String getRemarks() {
+		return remarks;
+	}
+
+	public void setRemarks(String remarks) {
+		this.remarks = remarks;
+	}
+
+	public String getRemarkspy() {
+		return remarkspy;
+	}
+
+	public void setRemarkspy(String remarkspy) {
+		this.remarkspy = remarkspy;
+	}
+	
+	
+	
+	
+	
+}
